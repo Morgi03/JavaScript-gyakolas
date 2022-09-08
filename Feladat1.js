5ed2416aec5730a75b620c6f0edefc889a47743a
Morgován Nimród <morgovan.nimrod@petrik.hu>
2022-09-08 12:33:34 +0200

    Tömb rendezése és elemeinek kiiratása

--- Feladat1.js
+++ Feladat1.js
@@ -2,3 +2,9 @@ let tomb = [];
 for (let i = 0; i < 10; i++) {
     tomb[i] = Math.floor((Math.random()*100)+1);
 }
+
+tomb2 = tomb.sort();
+
+for (let i = 0; i < tomb2.length; i++) {
+    console.log(tomb2[i]);
+}

48df61acc5e84a898e18cefd91762994f9eb2b87
Morgován Nimród <morgovan.nimrod@petrik.hu>
2022-09-08 13:05:08 +0200

    Javítás a tömb rendezésében

--- Feladat1.js
+++ Feladat1.js
@@ -3,7 +3,17 @@ for (let i = 0; i < 10; i++) {
     tomb[i] = Math.floor((Math.random()*100)+1);
 }
 
-tomb2 = tomb.sort();
+function osszehasonlit(a, b) {
+    if (a < b){
+        return 1;
+    } else if ( a > b){
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+tomb2 = tomb.sort(osszehasonlit);
 
 for (let i = 0; i < tomb2.length; i++) {
     console.log(tomb2[i]);

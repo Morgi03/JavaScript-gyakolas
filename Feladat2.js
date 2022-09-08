739e24bff60fb42fac17c0e9de4c0603e4c1fbdf
Morgován Nimród <morgovan.nimrod@petrik.hu>
2022-09-08 13:00:18 +0200

    Macskák tömb létrehozása, adatokkal való feltöltés

--- Feladat2.js
+++ Feladat2.js
@@ -1,3 +1,10 @@
 function randomszam(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+let Macskak = [];
+for (let i = 0; i < 20; i++) {
+  let macska = { nev : "Cirmos"+(i+1), eletkor : randomszam(2,10) } ;
+  Macskak[i] = macska;
+}
+
